7c94b24f3ee587dc535a81f7e9695ad215ee38da
AliGZN <geramiali.ag@gmail.com>
2023-09-09 15:50:45 +0330

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+
+        int odd = IsOdd.sumOdd(13,13);
+        System.out.println("Sum of odd numbers: " + odd);
+
+
+    }
+}
